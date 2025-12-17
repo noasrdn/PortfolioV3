d45c178c94ddd4af39ceccc86532bc8a3be76c2d
Noa ‎Serodon <noa.serodon@gmail.com>
2025-12-17 15:45:52 +0100

    Console Icon error

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/icon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
         {children}
